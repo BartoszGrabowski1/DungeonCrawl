8a186048ba1c3a00fc27cbb9b88accba4db01093
KZwolski <kkzwolski@gmail.com>
2022-08-22 14:25:13 +0200

    Movement validation

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Skeleton.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Skeleton.java
@@ -1,10 +1,16 @@
 package com.codecool.dungeoncrawl.logic.actors;
 
 import com.codecool.dungeoncrawl.logic.Cell;
+import com.codecool.dungeoncrawl.logic.GameMap;
 
 public class Skeleton extends Actor {
     public Skeleton(Cell cell) {
         super(cell);
+    }
+
+    @Override
+    public boolean checkIfMovePossible(int x, int y) {
+        return false;
     }
 
     @Override
