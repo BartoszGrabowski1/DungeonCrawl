ce380af3c974874d29ab15974d12c78e35b3f207
Ochota Dariusz <ochota.dariusz@gmail.com>
2022-08-22 15:17:42 +0200

    port to fxml

---
+++ src/main/java/com/codecool/dungeoncrawl/GameController.java
@@ -0,0 +1,11 @@
+package com.codecool.dungeoncrawl;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+
+public class GameController {
+
+    @FXML
+    public BorderPane borderpane;
+
+}
