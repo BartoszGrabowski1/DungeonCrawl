d673b4d4e82d3cc3d4d279d44e3638d07ba309da
Ochota Dariusz <ochota.dariusz@gmail.com>
2022-08-22 15:17:42 +0200

    port to fxml

--- src/main/java/com/codecool/dungeoncrawl/GameController.java
+++ src/main/java/com/codecool/dungeoncrawl/GameController.java
@@ -1,6 +1,7 @@
 package com.codecool.dungeoncrawl;
 
 import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
@@ -17,6 +18,8 @@ import java.util.ResourceBundle;
 public class GameController {
 
     @FXML
+    public BorderPane borderpane;
+
     private Canvas canvas = new Canvas(640, 640);
     @FXML
     private BorderPane borderpane = new BorderPane();
