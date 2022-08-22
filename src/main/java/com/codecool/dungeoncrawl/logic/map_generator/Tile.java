fa1f376c41aea3f15d08c8e57ffc1f5d259327fe
Ochota Dariusz <ochota.dariusz@gmail.com>
2022-08-22 14:30:49 +0200

    Map Generator

---
+++ src/main/java/com/codecool/dungeoncrawl/logic/map_generator/Tile.java
@@ -0,0 +1,22 @@
+package com.codecool.dungeoncrawl.logic.map_generator;
+
+public enum Tile {
+    STONE(" "),
+    FLOOR("."),
+    WALL("#");
+
+    private final String tile;
+
+    Tile(String tile) {
+        this.tile = tile;
+    }
+
+    public String getTile() {
+        return tile;
+    }
+
+    @Override
+    public String toString() {
+        return getTile();
+    }
+}
