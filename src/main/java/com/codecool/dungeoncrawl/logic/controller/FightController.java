93605c5e6cde1012ca52bbe6c39bb82228aa2b79
Matt <mattwasilewski96@gmail.com>
2022-08-23 14:10:28 +0200

    fight function in progress

--- src/main/java/com/codecool/dungeoncrawl/logic/controller/FightController.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/controller/FightController.java
@@ -6,6 +6,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FightController {
 
     @FXML
@@ -52,5 +55,48 @@ public class FightController {
         System.out.println("dupa");
     }
 
+
+    class Creature {
+        int hp;
+        int attackPower;
+        int mana;
+        int blockPower;
+
+        public Creature(int hp, int attackPower, int mana, int blockPower) {
+            this.hp = hp;
+            this.attackPower = attackPower;
+            this.mana = mana;
+            this.blockPower = blockPower;
+        }
+
+
+        Creature player = new Creature(100,20,50,10);
+        Creature monster = new Creature(60,10,10,10);
+
+    }
+
+
+    private enum Action {
+        ATTACK, CHARGE, BLOCK;
+
+        private static final Map<Action, Action> winMap = new HashMap<>();
+
+        static {
+            winMap.put(ATTACK, CHARGE);
+            winMap.put(CHARGE, BLOCK);
+            winMap.put(BLOCK, ATTACK);
+        }
+
+        ActionResult checkAgainst(Action action) {
+            if (this == action)
+                return ActionResult.DRAW;
+
+            return winMap.get(this) == action ? ActionResult.WIN : ActionResult.LOSE;
+        }
+
+        private enum ActionResult {
+            WIN, LOSE, DRAW
+        }
+    }
 }
 
