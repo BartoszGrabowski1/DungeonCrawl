e2b50954ceacba2fd6524c0697ea7ac798525796
BartoszGrabowski1 <heksargonh@gmail.com>
2022-08-23 10:08:30 +0200

    update

--- src/main/java/com/codecool/dungeoncrawl/logic/Items/Sword.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/Items/Sword.java
@@ -11,6 +11,7 @@ public class Sword extends Item{
 
     @Override
     public String getTileName() {
+        System.out.println("czemu ten pierdolony miecz sie nei wyswietla");
         return "sword";
     }
 }
--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Skeleton.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Skeleton.java
@@ -15,6 +15,7 @@ public class Skeleton extends Actor {
 
     @Override
     public String getTileName() {
+        System.out.println("szkielet korzysta");
         return "skeleton";
     }
 }
