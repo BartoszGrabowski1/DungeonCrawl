96db5f81d8ea7abebdc2c4e5a739b1ec776ddfda
Ochota Dariusz <ochota.dariusz@gmail.com>
2022-08-29 11:24:54 +0200

    refactor

--- src/main/java/com/codecool/dungeoncrawl/game/controller/ViewController.java
+++ src/main/java/com/codecool/dungeoncrawl/game/controller/ViewController.java
@@ -1,19 +1,11 @@
 package com.codecool.dungeoncrawl.game.controller;
 
 import com.codecool.dungeoncrawl.Main;
-import com.codecool.dungeoncrawl.Tiles;
-import com.codecool.dungeoncrawl.game.Cell;
-import com.codecool.dungeoncrawl.game.CellType;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
 
 import java.io.IOException;
-
-import static com.codecool.dungeoncrawl.Main.SCREEN_SIZE;
-import static com.codecool.dungeoncrawl.Main.map;
 
 public class ViewController {
 
@@ -64,39 +56,5 @@ public class ViewController {
         endViewLoader = new FXMLLoader(Main.class.getResource("end-view.fxml"));
         setView(endViewLoader, "Game Over");
     }
-
-//    public static void refreshView() {
-////        boolean playerOnItem = false;
-////        checkTile();
-//        context.setFill(Color.BLACK);
-//        context.fillRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
-//        for (int x = -SCREEN_SIZE; x < SCREEN_SIZE; x++) {
-//            for (int y = -SCREEN_SIZE; y < SCREEN_SIZE; y++) {
-//                try {
-//                    Cell cell = map.getCell(map.getPlayer().getX() + x - (SCREEN_SIZE / 2), map.getPlayer().getY() + y - (SCREEN_SIZE / 2));
-//                    if (cell.getCreature() != null) {
-//                        Tiles.drawTile(context, cell.getCreature(), x, y);
-////                        if (cell.getItem() != null && cell.getActor() instanceof Player) {
-////                            playerOnItem = true;
-////                        }
-//                    } else if (cell.getItem() != null) {
-//                        Tiles.drawTile(context, cell.getItem(), x, y);
-//                    } else {
-//                        Tiles.drawTile(context, cell, x, y);
-//                    }
-//                } catch (Exception e) {
-//                    Tiles.drawTile(context, new Cell(map, x, y, CellType.EMPTY), x, y);
-//                }
-//            }
-//        }
-////        if (playerOnItem) {
-////            showButton(pickUpItemBtn);
-////        } else {
-////            hideButton(pickUpItemBtn);
-////        }
-////
-////        gc.getHealthLabel().setText("" + map.getPlayer().getHealth());
-////        animation.play();
-//    }
 
 }
