c65d0d00d7640827445806a43c92fc8ab200c2b3
KZwolski <kkzwolski@gmail.com>
2022-09-18 22:09:19 +0200

    Basic tests

--- src/test/java/com/codecool/dungeoncrawl/game/CellTest.java
+++ src/test/java/com/codecool/dungeoncrawl/game/CellTest.java
@@ -1,31 +1,27 @@
-//package com.codecool.dungeoncrawl.game;
-//
-//<<<<<<< HEAD
-//=======
-//import com.codecool.dungeoncrawl.game.map.CellType;
-//import com.codecool.dungeoncrawl.game.map.GameMap;
-//>>>>>>> sprint_1
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class CellTest {
-//    GameMap map = new GameMap(3, 3, CellType.FLOOR);
-//
-//    @Test
-//    void getNeighbor() {
-//        Cell cell = map.getCell(1, 1);
-//        Cell neighbor = cell.getNeighbor(-1, 0);
-//        assertEquals(0, neighbor.getX());
-//        assertEquals(1, neighbor.getY());
-//    }
-//
-//    @Test
-//    void cellOnEdgeHasNoNeighbor() {
-//        Cell cell = map.getCell(1, 0);
-//        assertEquals(null, cell.getNeighbor(0, -1));
-//
-//        cell = map.getCell(1, 2);
-//        assertEquals(null, cell.getNeighbor(0, 1));
-//    }
-//}
+package com.codecool.dungeoncrawl.game;
+
+
+import com.codecool.dungeoncrawl.game.map.CellType;
+import com.codecool.dungeoncrawl.game.map.GameMap;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CellTest {
+    GameMap map = new GameMap(4, 4, CellType.FLOOR);
+
+    @Test
+    void getNeighbor() {
+        Cell cell = map.getCell(1, 1);
+        Cell neighbor = cell.getNeighbor(-1, 0);
+        assertEquals(0, neighbor.getX());
+        assertEquals(1, neighbor.getY());
+    }
+
+    @Test
+    void cellOnEdgeHasNoNeighbor() {
+        Cell cell  = map.getCell(1, 2);
+        assertEquals(map.getCell(1,3), cell.getNeighbor(0, 1));
+    }
+}
--- src/test/java/com/codecool/dungeoncrawl/game/CreatureTest.java
+++ src/test/java/com/codecool/dungeoncrawl/game/CreatureTest.java
@@ -1,57 +1,79 @@
-//package com.codecool.dungeoncrawl.game;
-//
-//import com.codecool.dungeoncrawl.Main;
-//import com.codecool.dungeoncrawl.game.creatures.Skeleton;
-//import com.codecool.dungeoncrawl.game.controller.NameController;
-//import com.codecool.dungeoncrawl.game.map.CellType;
-//import com.codecool.dungeoncrawl.game.map.GameMap;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class CreatureTest {
-//    GameMap gameMap = new GameMap(3, 3, CellType.FLOOR);
-//
-//    @Test
-//    void moveUpdatesCells() {
-//        Player player = new Player(gameMap.getCell(1, 1), NameController.getUserName());
-//        player.move(1, 0);
-//
-//        assertEquals(2, player.getX());
-//        assertEquals(1, player.getY());
-//        assertEquals(null, gameMap.getCell(1, 1).getCreature());
-//        assertEquals(player, gameMap.getCell(2, 1).getCreature());
-//    }
-//
-//    @Test
-//    void cannotMoveIntoWall() {
-//        gameMap.getCell(2, 1).setType(CellType.WALL);
-//        Player player = new Player(gameMap.getCell(1, 1), NameController.getUserName());
-//        player.move(1, 0);
-//
-//        assertEquals(1, player.getX());
-//        assertEquals(1, player.getY());
-//    }
-//
-//    @Test
-//    void cannotMoveOutOfMap() {
-//        Player player = new Player(gameMap.getCell(2, 1), NameController.getUserName());
-//        player.move(1, 0);
-//
-//        assertEquals(2, player.getX());
-//        assertEquals(1, player.getY());
-//    }
-//
-//    @Test
-//    void cannotMoveIntoAnotherActor() {
-//        Player player = new Player(gameMap.getCell(1, 1), NameController.getUserName());
-//        Skeleton skeleton = new Skeleton(gameMap.getCell(2, 1), Main.level);
-//        player.move(1, 0);
-//
-//        assertEquals(1, player.getX());
-//        assertEquals(1, player.getY());
-//        assertEquals(2, skeleton.getX());
-//        assertEquals(1, skeleton.getY());
-//        assertEquals(skeleton, gameMap.getCell(2, 1).getCreature());
-//    }
-//}
+package com.codecool.dungeoncrawl.game;
+
+import com.codecool.dungeoncrawl.game.controller.FightAction;
+import com.codecool.dungeoncrawl.game.creatures.Player;
+import com.codecool.dungeoncrawl.game.creatures.Skeleton;
+import com.codecool.dungeoncrawl.game.creatures.Vampire;
+import com.codecool.dungeoncrawl.game.map.CellType;
+import com.codecool.dungeoncrawl.game.map.GameMap;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreatureTest {
+    GameMap gameMap = new GameMap(3, 3, CellType.FLOOR);
+
+    @Test
+    void moveUpdatesCells() {
+        Player player = new Player(gameMap.getCell(1, 1),"Dev");
+        player.move(1, 0);
+
+        assertEquals(2, player.getX());
+        assertEquals(1, player.getY());
+        assertEquals(null, gameMap.getCell(1, 1).getCreature());
+        assertEquals(player, gameMap.getCell(2, 1).getCreature());
+    }
+
+    @Test
+    void cannotMoveIntoWall() {
+        gameMap.getCell(2, 1).setType(CellType.WALL);
+        Player player = new Player(gameMap.getCell(1, 1),"Dev");
+        player.move(1, 0);
+
+        assertEquals(1, player.getX());
+        assertEquals(1, player.getY());
+    }
+
+
+    @Test
+    void cannotMoveIntoAnotherActor() {
+        Player player = new Player(gameMap.getCell(1, 1),"Dev");
+        Skeleton skeleton = new Skeleton(gameMap.getCell(2, 1),1);
+        player.move(1, 0);
+
+        assertEquals(1, player.getX());
+        assertEquals(1, player.getY());
+        assertEquals(2, skeleton.getX());
+        assertEquals(1, skeleton.getY());
+        assertEquals(skeleton, gameMap.getCell(2, 1).getCreature());
+    }
+
+    @Test
+    void calculateDamageReturnProperFieldBasedOnProvidedAction(){
+        Player player = new Player(gameMap.getCell(1,1),"DEX");
+        player.setDamage(10);
+        player.setAbilityPower(20);
+        player.setBlockPower(30);
+        assertEquals(player.getDamage(), player.calcDamage(FightAction.ATTACK));
+        assertEquals(player.getAbilityPower(), player.calcDamage(FightAction.ABILITY));
+        assertEquals(player.getBlockPower(), player.calcDamage(FightAction.BLOCK));
+        assertEquals(0, player.calcDamage(FightAction.SPECIAL));
+    }
+    @Test
+    void getExpReturnsExperienceOfCreature(){
+        Player player = new Player(gameMap.getCell(2,2),"Dex");
+        player.setExp(10);
+        assertEquals(10, player.getExp());
+        assertNotEquals(120,player.getExp());
+    }
+
+    @Test
+    void getCreatureReturnsCreatureStandingOnSpecifiedCell(){
+        Player player = new Player(gameMap.getCell(1,1),"Dex");
+        Vampire vampire = new Vampire(gameMap.getCell(2,2),1);
+        Skeleton skeleton = new Skeleton(gameMap.getCell(0,0),1);
+        assertEquals(skeleton,gameMap.getCell(0,0).getCreature());
+        assertEquals(vampire,gameMap.getCell(2,2).getCreature());
+        assertEquals(player,gameMap.getCell(1,1).getCreature());
+    }
+}
