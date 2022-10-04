4acb81c9ddd978a674c4e25527049134d5975294
Matt <mattwasilewski96@gmail.com>
2022-10-05 00:48:51 +0200

    fixed

--- src/main/java/com/codecool/dungeoncrawl/game/map/Tiles.java
+++ src/main/java/com/codecool/dungeoncrawl/game/map/Tiles.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Tiles {
     public static int TILE_WIDTH = 32;
 
-    private static Image tileset = new Image("/com/codecool/dungeoncrawl/img/tiles.png", 2048, 3040, true, false);
+    private static Image tileset = new Image("/com/codecool/dungeoncrawl/img/tiles_player.png", 2048, 3040, true, false);
     private static Map<String, Tile> tileMap = new HashMap<>();
 
     public static class Tile {
